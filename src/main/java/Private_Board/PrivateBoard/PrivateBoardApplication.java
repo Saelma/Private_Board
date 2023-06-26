9218e23f905b664dd9484a4a7fdd725586e534b9
Saelma <rlarlgus300@gmail.com>
2023-06-27 03:47:30 +0900

    초기 세팅

---
+++ src/main/java/Private_Board/PrivateBoard/PrivateBoardApplication.java
@@ -0,0 +1,13 @@
+package Private_Board.PrivateBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Private_Board/PrivateBoard/PrivateBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Private_Board.PrivateBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
